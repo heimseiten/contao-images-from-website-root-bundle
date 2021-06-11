524963551bdfb980978ba80396e40b98b1211c3b
Niels <info@heimseiten.de>
2021-06-11 22:45:58 +0200

    Update tl_layout.php

--- src/Resources/contao/dca/tl_layout.php
+++ src/Resources/contao/dca/tl_layout.php
@@ -4,10 +4,10 @@ use Contao\Config;
 use Contao\CoreBundle\DataContainer\PaletteManipulator;
 
 PaletteManipulator::create()
-    ->addLegend('root_img_legend', 'sections_legend', PaletteManipulator::POSITION_AFTER)
-    ->addField('root_img_logo_size',  'root_img_legend', PaletteManipulator::POSITION_APPEND)
-    ->addField('root_img_headerbg_size',  'root_img_legend', PaletteManipulator::POSITION_APPEND)
-    ->addField('root_img_custom_size',  'root_img_legend', PaletteManipulator::POSITION_APPEND)
+    ->addLegend('root_img_size_legend', 'sections_legend', PaletteManipulator::POSITION_AFTER)
+    ->addField('root_img_logo_size',  'root_img_size_legend', PaletteManipulator::POSITION_APPEND)
+    ->addField('root_img_headerbg_size',  'root_img_size_legend', PaletteManipulator::POSITION_APPEND)
+    ->addField('root_img_custom_size',  'root_img_size_legend', PaletteManipulator::POSITION_APPEND)
     ->applyToPalette('default', 'tl_layout') 
 ;
 
@@ -15,21 +15,21 @@ $GLOBALS['TL_DCA']['tl_layout']['fields'] += [
     'root_img_logo_size' => [
         'inputType'        => 'imageSize',
         'options'          => \System::getImageSizes(),
-        'reference'        => &$GLOBALS['TL_LANG']['MSC'],
+        'reference'        => &$GLOBALS['TL_LANG']['tl_layout'],
         'eval'             => array( 'rgxp' => 'digit', 'includeBlankOption' => true, 'tl_class'  => 'clr' ),
         'sql'              => "varchar(64) NOT NULL default ''"
     ],
     'root_img_headerbg_size' => [
         'inputType'        => 'imageSize',
         'options'          => \System::getImageSizes(),
-        'reference'        => &$GLOBALS['TL_LANG']['MSC'],
+        'reference'        => &$GLOBALS['TL_LANG']['tl_layout'],
         'eval'             => array( 'rgxp' => 'digit', 'includeBlankOption' => true, 'tl_class'  => 'clr' ),
         'sql'              => "varchar(64) NOT NULL default ''"
     ],
     'root_img_custom_size' => [
         'inputType'        => 'imageSize',
         'options'          => \System::getImageSizes(),
-        'reference'        => &$GLOBALS['TL_LANG']['MSC'],
+        'reference'        => &$GLOBALS['TL_LANG']['tl_layout'],
         'eval'             => array( 'rgxp' => 'digit', 'includeBlankOption' => true, 'tl_class'  => 'clr' ),
         'sql'              => "varchar(64) NOT NULL default ''"
     ],
